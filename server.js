bb177c0408dfa1ba72cfba72d617811cd36fb5dc
Pashall KAMANGA <swag60693@gmail.com>
2026-03-27 23:05:38 +0200

    Chat backend ready

--- server.js
+++ server.js
@@ -4,14 +4,44 @@ const { Server } = require("socket.io");
 const cors = require("cors");
 
 const app = express();
-app.use(cors());
-app.use(express.json());
 
-let messages = [];
-app.get("/messages",(req,res)=>res.json(messages));
+app.use(cors({
+  origin: "*", // allow all for now (we lock later)
+}));
 
 const server = http.createServer(app);
-const io = new Server(server,{cors:{origin:"*"}});
-io.on("connection",socket=>{socket.on("sendMessage",msg=>{messages.push(msg); io.emit("newMessage",msg);});});
 
-server.listen(3000,()=>console.log("Chat server running on port 3000"));
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+  },
+});
+
+let messages = [];
+
+io.on("connection", (socket) => {
+  console.log("User connected");
+
+  // Send old messages
+  socket.emit("load_messages", messages);
+
+  socket.on("send_message", (data) => {
+    messages.push(data);
+    io.emit("receive_message", data);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("User disconnected");
+  });
+});
+
+// simple test route
+app.get("/", (req, res) => {
+  res.send("Backend is running");
+});
+
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, "0.0.0.0", () => {
+  console.log("Server running on port " + PORT);
+});
